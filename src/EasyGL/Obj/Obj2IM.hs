f173e5e851c88c16a346201857988d63e98c61bc
JoseD92 <jose_daniel_d@hotmail.com>
2017-07-23 04:18:55 -0400

    Obj2IM now uses vectors, faster, less memory usage but code harder to read

--- src/EasyGL/Obj/Obj2IM.hs
+++ src/EasyGL/Obj/Obj2IM.hs
@@ -16,74 +16,113 @@ module EasyGL.Obj.Obj2IM (
 toIndexedModel
   )
 where
-import EasyGL.Obj.ObjData
-import Graphics.Rendering.OpenGL
-import Data.Array
-import qualified EasyGL.IndexedModel as IM
-import qualified Data.Sequence as Seq
-import qualified Data.Map.Strict as Map
-import Data.Foldable (toList)
-import qualified Data.Vector.Storable as VS
-import qualified Data.Maybe as M
 
-toVector :: (VS.Storable a) => [a] -> VS.Vector a
-toVector = VS.fromList
+import           Control.Monad.Reader
+import           Control.Monad.ST
+import           Control.Monad.State.Lazy
+import           Data.Foldable                (toList)
+import qualified Data.Map.Strict              as Map
+import qualified Data.Maybe                   as M
+import qualified Data.Sequence                as Seq
+import qualified Data.Vector as V
+import qualified Data.Vector.Mutable as VM
+import qualified Data.Vector.Storable         as VS
+import qualified Data.Vector.Storable.Mutable as VSM
+import qualified EasyGL.IndexedModel          as IM
+import           EasyGL.Obj.ObjData
+import           Graphics.Rendering.OpenGL    hiding (get)
 
--- | Transforms a Obj Group into an indexed model.
+defaultNormal :: Maybe (Vector3 GLfloat) -> Vector3 GLfloat
+defaultNormal (Just x) = x
+defaultNormal Nothing = Vector3 0 0 0
+
+defaultTexture :: Maybe (Vector2 GLfloat) -> Vector2 GLfloat
+defaultTexture (Just x) = x
+defaultTexture Nothing = Vector2 0 0
+
+-- | A needed correction to all textureCoord, as image loading library loads everything upside down, and it is easier to correct here.
+easyGLVector2Correction :: Vector2 GLfloat -> Vector2 GLfloat
+easyGLVector2Correction (Vector2 x y) = Vector2 x (1-y)
+
 toIndexedModel :: Group -> IM.IndexedModel
-toIndexedModel g = IM.IndexedModel (toVector vert2) (toVector norm2) (toVector $ map invertY tex1) (toVector index2)
+toIndexedModel g
+    | M.isNothing norm0 && M.isNothing tex0 = IM.IndexedModel (V.convert vert0) VS.empty VS.empty (VS.fromList index0)
+    | M.isJust norm0 && M.isNothing tex0 = runST $ do
+      (newVert,newNorms,newIndex) <- rearrangeCaller defaultNormal vert0 (V.fromList index0) (V.fromList . toList . M.fromJust $ norm0) (V.fromList normIndex0)
+      return $ IM.IndexedModel (V.convert newVert) (V.convert newNorms) VS.empty (V.convert newIndex)
+    | M.isNothing norm0 && M.isJust tex0 = runST $ do
+      (newVert,newText,newIndex) <- rearrangeCaller defaultTexture vert0 (V.fromList index0) (V.fromList . toList . M.fromJust $ tex0) (V.fromList textIndex0)
+      return $ IM.IndexedModel (V.convert newVert) VS.empty (V.convert . V.map easyGLVector2Correction $ newText) (V.convert newIndex)
+    | M.isJust norm0 && M.isJust tex0 = runST $ do
+      (newVert,newNorms,newIndex) <- rearrangeCaller defaultNormal vert0 (V.fromList index0) (V.fromList . toList . M.fromJust $ norm0) (V.fromList normIndex0)
+      let newThing = V.zip newVert newNorms
+      (newThing,newText,newIndex) <- rearrangeCaller defaultTexture newThing newIndex (V.fromList . toList . M.fromJust $ tex0) (V.fromList textIndex0)
+      let (newVert,newNorms) = V.unzip newThing
+      return $ IM.IndexedModel (V.convert newVert) (V.convert newNorms) (V.convert . V.map easyGLVector2Correction $ newText) (V.convert newIndex)
+    | otherwise = IM.emptyIndexedModel
   where
-    vert0 = toList $ vertices g
+    vert0 = V.fromList . toList $ vertices g
     norm0 = normals g
     tex0 = textureCoord g
-    index0 = indexes g
-    (vert1,norm1,index1) = if M.isNothing norm0
-      then (vert0,[],index0)
-      else addNorms vert0 index0 (toList . M.fromJust $ norm0) (normalsIndex g)
-    (vert2,norm2,tex1,index2) = if M.isNothing tex0
-      then (vert1,norm1,[],index1)
-      else addTexCoords vert1 index1 norm1 (toList . M.fromJust $ tex0) (textureCoordIndex g)
-    invertY (Vector2 x y) = Vector2 x (1-y) -- either my image loading library is lying to me, or i actually have to make this transformation, but it works!!!
+    (index0,textIndex0,normIndex0) = allIndexes g
 
-addNorms :: [Vertex3 GLfloat] -> [GLuint] -> [Vector3 GLfloat] -> [GLuint] -> ([Vertex3 GLfloat],[Vector3 GLfloat],[GLuint])
-addNorms verts vertsIndex norms normsIndex = rearrange 0 indexLen vertsLen vertsLookup vertsIndexLookup normsLookup normsIndexLookup resul Seq.empty Seq.empty Map.empty (Vector3 0 0 0)
-  where
-    vertsLen = fromIntegral $ length verts
-    normsLen = fromIntegral $ length norms
-    indexLen = fromIntegral $ length vertsIndex -- vertsIndex and normsIndex have the same length
-    vertsLookup = listArray (0,vertsLen-1) verts
-    vertsIndexLookup = listArray (0,indexLen-1) vertsIndex
-    normsLookup = listArray (0,normsLen-1) norms
-    normsIndexLookup = listArray (0,indexLen-1) normsIndex
-    resul = listArray (0,vertsLen-1) $ repeat Nothing
+rearrangeCaller :: (Ord a,Ord b,Integral c,Num c) => (Maybe b -> b) -> V.Vector a -> V.Vector c -> V.Vector b -> V.Vector c -> ST s (V.Vector a,V.Vector b,V.Vector c)
+rearrangeCaller f vectorA indexA vectorB indexB = do
+  mutableIndex <- V.thaw indexA
+  acc <- VM.replicate (V.length vectorA) Nothing
+  let reader = RearrangeRead vectorA vectorB acc mutableIndex indexB
+  (RearrangeState _ extraA extraB _) <- runReaderT (execStateT rearrange defaultRearrangeState) reader
+  freezed <- V.freeze acc
+  let returnA = vectorA V.++ (V.fromList . toList $ extraA)
+      returnB = V.map f freezed V.++ (V.fromList . toList $ extraB)
+  returnC <- V.freeze mutableIndex
+  return (returnA,returnB,returnC)
 
-addTexCoords :: [Vertex3 GLfloat] -> [GLuint] -> [Vector3 GLfloat] -> [Vector2 GLfloat] -> [GLuint] -> ([Vertex3 GLfloat],[Vector3 GLfloat],[Vector2 GLfloat],[GLuint])
-addTexCoords verts vertsIndex norms texCood texCoodIndex = (newVerts,newNorm,newtexCood,newIndex)
-  where
-    vertsLen = fromIntegral $ length verts
-    texCoodLen = fromIntegral $ length texCood
-    indexLen = fromIntegral $ length vertsIndex -- vertsIndex and normsIndex have the same length
-    dataLookup = listArray (0,vertsLen-1) $ zip verts norms
-    dataIndexLookup = listArray (0,indexLen-1) vertsIndex
-    texCoodLookup = listArray (0,texCoodLen-1) texCood
-    texCoodIndexLookup = listArray (0,indexLen-1) texCoodIndex
-    resul = listArray (0,vertsLen-1) $ repeat Nothing
-    (newData,newtexCood,newIndex) = rearrange 0 indexLen vertsLen dataLookup dataIndexLookup texCoodLookup texCoodIndexLookup resul Seq.empty Seq.empty Map.empty (Vector2 0 0)
-    (newVerts,newNorm) = unzip newData
+data RearrangeState a b c = RearrangeState {
+  current :: Int,
+  extraA :: Seq.Seq a,
+  extraB :: Seq.Seq b,
+  locator :: Map.Map (a,b) c
+}
 
-rearrange :: (Ord a,Ord b) => GLuint -> GLuint -> GLuint -> Array GLuint a -> Array GLuint GLuint -> Array GLuint b -> Array GLuint GLuint -> Array GLuint (Maybe b) -> Seq.Seq a -> Seq.Seq b -> Map.Map (a,b) GLuint -> b -> ([a],[b],[GLuint])
-rearrange current limit next dataLookup dataIndexLookup newDataLookup newDataIndexLookup resul extraA extraB extraMap elemB
-  | current == limit = (elems dataLookup ++ toList extraA,getMaybeArr resul ++ toList extraB,elems dataIndexLookup)
-  | otherwise = rearrange (current+1) limit new_Next dataLookup new_DataIndexLookup newDataLookup newDataIndexLookup new_Resul new_extraA new_extraB new_extraMap elemB
-  where
-    getMaybeArr = map (maybe elemB id) . elems
-    currentData = dataLookup ! (dataIndexLookup ! current)
-    currentNewData = newDataLookup ! (newDataIndexLookup ! current)
-    currentResul = resul ! (dataIndexLookup ! current)  -- its a maybe
-    isItInAnyExtra = Map.lookup (currentData,currentNewData) extraMap -- its a maybe
-    new_Next = maybe next (const $ maybe (next+1) (const next) isItInAnyExtra) currentResul
-    new_DataIndexLookup = maybe dataIndexLookup (const $ maybe (dataIndexLookup // [(current,next)]) (\i -> dataIndexLookup // [(current,i)]) isItInAnyExtra) currentResul
-    new_Resul = maybe (resul // [(dataIndexLookup ! current,Just currentNewData)]) (const resul) currentResul
-    new_extraA = maybe extraA (const $ maybe (extraA Seq.|> currentData) (const extraA) isItInAnyExtra) currentResul
-    new_extraB = maybe extraB (const $ maybe (extraB Seq.|> currentNewData) (const extraB) isItInAnyExtra) currentResul
-    new_extraMap = maybe extraMap (const $ maybe (Map.insert (currentData,currentNewData) next extraMap) (const extraMap) isItInAnyExtra) currentResul
+defaultRearrangeState :: RearrangeState a b c
+defaultRearrangeState = RearrangeState 0 Seq.empty Seq.empty Map.empty
+
+data RearrangeRead a b c s = RearrangeRead {
+  readVetorA :: V.Vector a,
+  readVetorB :: V.Vector b,
+  resulVector :: VM.MVector s (Maybe b),
+  indexA :: VM.MVector s c,
+  indexB :: V.Vector c
+}
+
+type RearrangeM a b c s = StateT (RearrangeState a b c) (ReaderT (RearrangeRead a b c s) (ST s)) ()
+
+rearrange :: (Ord a,Ord b,Integral c,Num c) => RearrangeM a b c s
+rearrange = do
+  readdata <- ask
+  state <- get
+  unless (current state == VM.length (indexA readdata)) $ do
+    currentIndexA <- VM.read (indexA readdata) (fromIntegral $ current state)
+    let currentIndexB = indexB readdata V.! fromIntegral (current state)
+        currentA = readVetorA readdata V.! fromIntegral currentIndexA
+        currentB = readVetorB readdata V.! fromIntegral currentIndexB
+    currentMaybeB <- VM.read (resulVector readdata) (fromIntegral currentIndexA)
+    if M.isNothing currentMaybeB then
+      VM.write (resulVector readdata) (fromIntegral currentIndexA) (Just currentB)
+    else
+      unless (currentB == M.fromJust currentMaybeB) $ do
+        let search = Map.lookup (currentA,currentB) $ locator state
+        case search of
+          Just x -> VM.write (indexA readdata) (fromIntegral $ current state) x
+          Nothing -> do
+            let newIndex = fromIntegral $ V.length (readVetorA readdata) + Seq.length (extraA state)
+            VM.write (indexA readdata) (fromIntegral $ current state) newIndex
+            modify' (\s-> s{
+                  extraA=extraA s Seq.|> currentA,
+                  extraB=extraB s Seq.|> currentB,
+                  locator=Map.insert (currentA,currentB) newIndex (locator s)
+                }
+              )
+    modify' (\s->s{current=current state + 1})
+    rearrange
